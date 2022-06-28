8a04878ec3302cfccb717efd345f629939ee4e6b
liuqi <liuqi@stdb.io>
2022-06-28 17:16:58 +0800

    remove unused overloaded deduce guide

--- source/generator_cpp.cpp
+++ source/generator_cpp.cpp
@@ -382,7 +382,6 @@ void GeneratorCpp::GenerateVariantVisitHelper_Header()
 {
     std::string code = R"CODE(
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 )CODE";
 
     // Prepare code template
