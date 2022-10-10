e86a785e638c7c22525b731d387f3f8a8d572b38
lovio <lovio@users.noreply.github.com>
2022-10-10 09:52:14 +0000

    Update generated documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -10,6 +10,6 @@ var searchData=
   ['addvalue_212',['AddValue',['../struct_f_b_e_1_1_enum_body.html#a6e5dfd73a4a7cb509c44a085a391f087',1,'FBE::EnumBody::AddValue()'],['../struct_f_b_e_1_1_flags_body.html#a2fa3b0930898923930f1366eecbd5544',1,'FBE::FlagsBody::AddValue()'],['../struct_f_b_e_1_1_variant_body.html#a848fbcba744d0ec2f551fdd54b17d84c',1,'FBE::VariantBody::AddValue()']]],
   ['addvariant_213',['AddVariant',['../struct_f_b_e_1_1_statements.html#a17dd6d7e734f0ba16334c3ef9e10a036',1,'FBE::Statements']]],
   ['arena_214',['Arena',['../class_f_b_e_1_1_generator_cpp.html#a00f27378a61a699228c1bb21979fb82c',1,'FBE::GeneratorCpp::Arena() const noexcept'],['../class_f_b_e_1_1_generator_cpp.html#ab6038da278c1be002c4033b108caa29d',1,'FBE::GeneratorCpp::Arena(bool arena) noexcept']]],
-  ['arenaheader_215',['ArenaHeader',['../class_f_b_e_1_1_generator_cpp.html#a3b20853bf8dbeb8d892edf7643df89f5',1,'FBE::GeneratorCpp::ArenaHeader() const noexcept'],['../class_f_b_e_1_1_generator_cpp.html#a2f98cff6398bcd9376bbd06b14a2b0ad',1,'FBE::GeneratorCpp::ArenaHeader(const std::string &amp;arena_header) noexcept']]],
-  ['arenatags_216',['ArenaTags',['../class_f_b_e_1_1_generator_cpp.html#a2231d9cccf321de029291ac57c11f8d5',1,'FBE::GeneratorCpp::ArenaTags() const noexcept'],['../class_f_b_e_1_1_generator_cpp.html#a3a4f6fff60ec217de44395bc0a553cb8',1,'FBE::GeneratorCpp::ArenaTags(const std::list&lt; std::string &gt; &amp;arena_tags) noexcept']]]
+  ['arenaheader_215',['ArenaHeader',['../class_f_b_e_1_1_generator_cpp.html#a3b20853bf8dbeb8d892edf7643df89f5',1,'FBE::GeneratorCpp']]],
+  ['arenatags_216',['ArenaTags',['../class_f_b_e_1_1_generator_cpp.html#a8cd3a295915a8e3bd8b4e6482507af4c',1,'FBE::GeneratorCpp']]]
 ];
